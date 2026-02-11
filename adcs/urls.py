84e033f5ab50697f56c7706f3df0ae689daa0658
Your Name <eanishadhikarii@gmail.com>
2026-02-11 10:54:13 +0545

    Add navbar

--- adcs/urls.py
+++ adcs/urls.py
@@ -21,6 +21,7 @@ from temp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home),
+    path('about/',views.about),
     
 ]
--- temp/views.py
+++ temp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'temp/home.html')
+
+def about(request):
+    return render(request, 'temp/about.html')
